b4be68de5cf6da0d12d928126b0362bcf7e046b2
arahman1783 <a.rahman1783@yahoo.com>
2020-10-05 09:56:21 +0200

    fix api

--- src/apis/currentTemp.js
+++ src/apis/currentTemp.js
@@ -1,5 +1,5 @@
 export default async function getCurrentTemp (city) {
-  let response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c3893d1cc91c7d2a402863d4c414be54&units=metric`);
+  let response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=c3893d1cc91c7d2a402863d4c414be54&units=metric`);
   // return response;
   let data = await response.json();
   return data
