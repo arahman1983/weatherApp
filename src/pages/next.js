6937546758fa49de6aff09ec0739182aeacf7aaf
arahman1783 <a.rahman1783@yahoo.com>
2020-10-05 16:20:53 +0200

    fix warniungs

--- src/pages/next.js
+++ src/pages/next.js
@@ -11,6 +11,7 @@ export default function Next() {
     let currentDate = new Date();
     selectedCity && getNextTemp(selectedCity.name).then((data) => {
       if(data.list.length>0){
+          // eslint-disable-next-line array-callback-return
           let dataList = data.list.filter(item => {
           let itemDate = new Date(item.dt * 1000);
           if(itemDate.getDay() !== currentDate.getDay()){
